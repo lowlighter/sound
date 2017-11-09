278aa4692c763c18b5f4508a09f9b77c252bfd5e
Lecoq Simon <simon.lecoq@live.fr>
2017-11-09 13:13:06 +0100

    Draft using correlation to check if words are the same

--- bin/compare.py
+++ bin/compare.py
@@ -4,6 +4,8 @@
 # > persons : Liste des personnes prononçant ces mots
 # > compare_format : Format des fichiers (doit contenir {person} et {word} dans le nom)
 def compare(words, persons, time_res=0, amp_res=0, fmin=0, fmax=0, fcs=False, nb_filters=0, q=0, n=0, filters=[], filters_fq=[], compare_format=""):
+    # Initialisation
+    rseqs = []
     # Durée du fichier le plus long
     xlim = 0
     for word in words:
@@ -12,11 +14,20 @@ def compare(words, persons, time_res=0, amp_res=0, fmin=0, fmax=0, fcs=False, nb
             xlim = max(xlim, len(y)/fs-time_res)
 
     # Traitement
+    mx = 0
     for word in words:
         for person in persons:
-            compute(
+            d, d, rseq = compute(
                 file=compare_format.format(person=person, word=word), spec_only="{word} - {person}".format(person=person, word=word),
                 q=q, n=n, fcs=fcs, nb_filters=nb_filters, fmin=fmin, fmax=fmax,
                 filters=filters, filters_fq=filters_fq,
                 time_res=time_res, amp_res=amp_res, spec_xlim=xlim
             )
+            rseqs.append(rseq)
+            mx = max(mx, len(rseq[0]))
+
+    for i in range(len(rseqs)):
+        for j in range(len(rseqs[i])):
+            rseqs[i][j].resize(mx)
+
+    return rseqs
