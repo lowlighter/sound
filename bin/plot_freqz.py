c2ca59d991f5a5ce802babf74383280a8eb336a4
Lecoq Simon <simon.lecoq@live.fr>
2017-12-16 11:55:28 +0100

    Improved freqz function

--- bin/plot_freqz.py
+++ bin/plot_freqz.py
@@ -7,16 +7,17 @@ def plot_freqz(filters, fs):
     plt.figure(figsize=(12, 6), dpi= 80, facecolor="w", edgecolor="k")
     for i in range(len(filters)):
         # Réponse fréquentielle
+        nyq = fs / 2
         b, a = filters[i]
-        w, h = signal.freqz(b, a)
+        w, h = signal.freqz(b, a, worN=16384)
         # Affichage
-        plt.semilogx((fs/(2*np.pi))*w, 20 * np.log10(abs(h)));
+        plt.semilogx(nyq*w/np.pi, 20 * np.log10(abs(h)));
 
     # Affichage
     plt.title("Réponse fréquentielle de la banque de filtre")
     plt.xlabel("Fréquence [Hz]")
     plt.ylabel("Gain [dB]")
-    plt.xlim([0, 20000])
-    plt.ylim([-100, 0])
+    plt.xlim([0, 24000])
+    plt.ylim([-60, 0])
     plt.grid(which="both", axis="both")
     plt.show()
